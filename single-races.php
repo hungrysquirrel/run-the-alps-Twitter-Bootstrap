479494c83cb1afb6e904ccd65d0bae2288bb52f8
Josh Rubinstein <joshmaxrubinstein@gmail.com>
2017-08-09 21:05:24 -0700

    race detail page basic layout

--- single-races.php
+++ single-races.php
@@ -16,8 +16,7 @@ get_header(); ?>
       <div class="col-xl-12">
       
         <div class="row">
-          <div class="col-xl-12 blog-post-top-rule">
-           
+          <div class="col-xl-8 offset-xl-2 blog-post-top-rule">
             <header class="page-title overview overview-without-border">
               <h1><?php the_title();?></h1>
             </header>
@@ -25,87 +24,89 @@ get_header(); ?>
         </div>
             
         <div class="row"> 
-           <div class="col-xl-8">
-              <div class="races">
-                <div class="row race-overview">
-                  <div class="col-xl-12 race-overview-top-border">
-                    <div class="row">
-                      <div class="col-xl-4">
-                        <a href="www.snowwalkrun.ch">
-                          <img class="img-fluid" src="<?php the_field('race_promo_photo'); ?>" alt="<?php the_title();?>">
-                        </a>
-                      </div>
-                      <div class="col-xl-8">
-                        <div class="row">
-                          <div class="col-xl-6">
-                            <ul class="race-info">
-                              <li>
-                                <small class="text-muted">Name / Location</small>
-                                <h2><?php the_title();?></h2>
-                              </li>
-                              <li>
-                                <small class="text-muted">Date</small>
-                                <?php the_field('race_date'); ?>
-                              </li>
-                              <li>
-                                <small class="text-muted">Date</small>
-                                <?php the_field('race_end_date'); ?>
-                              </li>
-                            </ul>
-                          </div>
-                          <div class="col-xl-6">
-                            <ul class="race-info">
-                              <li>
-                                <small class="text-muted">Distance</small> 
-                                <?php the_field('race_distance'); ?>
-                              </li>
-                              <li>
-                                <small class="text-muted">Vertical</small> 
-                                <?php the_field('race_vertical'); ?>
-                              </li>
-                            </ul>
-                          </div>
-                        </div>
+          <div class="col-xl-8 offset-xl-2">
+            <div class="races">
+              <div class="row race-overview">
+                <div class="col-xl-12">
+                  <div class="row">
+                    
+                    <div class="col-xl-12">
+                      <a href="<?php the_field('race_website'); ?>">
+                        <img class="img-fluid" src="<?php the_field('race_promo_photo'); ?>" alt="<?php the_title();?>">
+                      </a>
+                    </div>
+
+                    <div class="col-xl-12">
+                      <div class="row">
                         
-                        <div class="row">
-                          <div class="col-xl-12">
-                            <div class="row">
-                              <div class="col-xl-6">
-                                <p class="race-info">
-                                  <small class="text-muted">Race website</small>
-                                  <a href="<?php the_field('race_website'); ?>"><?php the_field('race_website'); ?></a>
-                                </p>
-                              </div>
-                              <div class="col-xl-6">
-                                <p class="race-info">
-                                  <small class="text-muted">Country</small>
-                                  <?php the_field('race_country'); ?>
-                                </p>
-                              </div>
+                      
+                        <div class="col-xl-6">
+                          <div class="row">
+                            <div class="col-xl-12">
+                              <p class="race-info">
+                                <small class="text-muted">Date</small>
+                                  <?php $date = DateTime::createFromFormat('Ymd', get_field('race_date', $child->ID)); print $date->format('m/d/Y')?>
+                                  <?php if(get_field('race_end_date')): ?>&ndash; <?php $date = DateTime::createFromFormat('Ymd', get_field('race_end_date', $child->ID)); print $date->format('m/d/Y')?>
+                                  <?php endif; ?>
+                              </p>
+                            </div>
+                            <div class="col-xl-12">
+                              <p class="race-info">
+                                <small class="text-muted">Country</small>
+                                <?php the_field('race_country'); ?>
+                              </p>
+                            </div>
+                            <div class="col-xl-12">
+                              <p class="race-info">
+                                <small class="text-muted">Race website</small>
+                                <a href="<?php the_field('race_website'); ?>"><?php the_field('race_website'); ?></a>
+                              </p>
                             </div>
                           </div>
                         </div>
-                        <div class="row"><div class="col-xl-12">
-                          <?php the_field('race_description'); ?>
+                        
+                        
+                        <div class="col-xl-6">
+                          <div class="row">
+                            <div class="col-xl-12">
+                              <p class="race-info">
+                                <small class="text-muted">Distance</small> 
+                                <?php the_field('primary_race_distance'); ?>
+                                <?php if (get_field('secondary_race_distances')): ?>
+                                  ,<?php the_field('secondary_race_distances'); ?>
+                                <?php endif; ?>
+                              </p>
+                            </div>
+                            <div class="col-xl-12">
+                              <p class="race-info">
+                                <small class="text-muted">Elevation</small> 
+                                <?php the_field('race_vertical'); ?>
+                              </p>
+                            </div>
+                          </div>
                         </div>
+
                       </div>
+                    </div>
+                    
+                    <div class="col-xl-12">
+                      <p class="race-info my-0">
+                        <small class="text-muted">Description</small>
+                      </p>
+                      <?php the_field('race_details'); ?>
                     </div>
                   </div>
                 </div>
-              </div>
-            </div><!--races-->  
-           </div>
 
-           <div class="col-xl-4">
-             <div class="sidebar-nav race-nav">
-               <div class="month-heading">Placeholder</div>
-             </div>
-           </div>
+              </div><!--/race-overview-->
+            </div><!--/races-->
+          </div>
         </div>
-          
-      </div><!-- /.span12 -->
-        <?php get_footer(); ?>
-      </div><!--/row-->  
-    </div><!-- /container -->
-  </body>
+
+      </div>
+    </div><!-- /.span12 -->
+    <?php get_footer(); ?>
+  </div><!--/row-->  
+
+</body>
 </html>
